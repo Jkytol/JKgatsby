c0e1d868bdd9753a24a01159ab70c2a0aafdf601
Janne Kytölä <50573855+Jkytol@users.noreply.github.com>
2020-09-21 14:38:10 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Developer" keywords={[`Janne`, `Kytola`, `jannekytola`]} />
     <LandingBio />
   </Layout>
 )
