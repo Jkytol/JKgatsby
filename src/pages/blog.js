e411217532173ef3f8b99922d1ecb060b38f1d84
Janne Kytölä <50573855+Jkytol@users.noreply.github.com>
2020-10-04 21:42:43 +0300

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -63,17 +63,6 @@ const IndexPage = ({ data }) => {
               <p>{node.excerpt}</p>
             </div>
           ))}
-<form name="Contact Form" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="Contact Form" />
-      <div>
-        <label>Your Email:</label>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <label>Message:</label>
-        <textarea name="message" />
-      </div>
-      <button type="submit">Send</button></form>
       </Content>
     </Layout>
   )
