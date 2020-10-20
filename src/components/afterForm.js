5514b1f232b59cc4d38720486a4436437ea9ce04
Hey Yo! <50573855+Jkytol@users.noreply.github.com>
2020-10-20 19:23:55 +0300

    Update afterForm.js

--- src/components/afterForm.js
+++ src/components/afterForm.js
@@ -1,13 +1,6 @@
-// Adapted from:
-// https://github.com/sw-yx/gatsby-netlify-form-example-v2/blob/master/src/pages/contact.js
-// https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
-
-// This is what we'll use to navigate to the custom success page
-// More on this here: https://www.gatsbyjs.org/docs/gatsby-link/#how-to-use-the-navigate-helper-function
 import { navigate } from "gatsby"
 import React, { useState } from "react";
 
-// This function encodes the captured form data in the format that Netlify's backend requires
 function encode(data) {
     return Object.keys(data)
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
@@ -22,10 +15,8 @@ const NameForm = (props) => {
   }
 
   const handleSubmit = (event) => {
-    // Prevent the default onSubmit behavior
+
     event.preventDefault();
-    // POST the encoded form with the content-type header that's required for a text submission
-    // Note that the header will be different for POSTing a file
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -34,15 +25,12 @@ const NameForm = (props) => {
         ...name
       })
     })
-      // On success, redirect to the custom success page using Gatsby's `navigate` helper function
-      .then(() => navigate("/Thank-you/"))
-      // On error, show the error in an alert
+
+      .then(() => navigate("/thank-you/"))
       .catch(error => alert(error));
   };
-
-  return (
     
-
+  return (
 <div id="myDIV">
 <p>Message sent succesfully! :)</p>
 <br></br>
@@ -50,10 +38,6 @@ const NameForm = (props) => {
 <br></br>
 <p> have a nice day!</p>
 </div>
-
-
-
-
   );
 }
 
