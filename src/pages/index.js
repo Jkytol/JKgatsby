086823ea17561b274946e9d886079c4c6a73cd16
Hey Yo! <50573855+Jkytol@users.noreply.github.com>
2020-10-20 19:01:46 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,27 +3,15 @@ import React from "react"
 import LandingBio from "../components/landing-bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import NameForm from "../components/form"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Developer" keywords={[`Janne`, `Kytola`, `jannekytola`, `KytöläJanne`, `parasSeo`, `JanneSovelluskehitys`, `UnityPelinkehitys`]} />
     <LandingBio />
 
-  
-  <div id="myDIV">
-  <p>Looking to get in touch?</p>
- <form name="Contact Form" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="Contact Form"  />
-      <div>
-        <label></label>
-        <input type="email" name="email" placeholder="Email" />
-      </div>
-      <div>
-        <label></label>
-        <textarea name="message" placeholder="Message" />
-      </div>
-      <button type="submit">Send</button></form>
-</div>
+    <NameForm />
+
 <div id="myDIVi"></div>
 <div id="myDIVii"><a href="https://paypal.me/kahviraha/5"><img src="https://raw.githubusercontent.com/Jkytol/JKgatsby/master/src/images/cGIF.gif" title="donateButton" /></a></div>
 
