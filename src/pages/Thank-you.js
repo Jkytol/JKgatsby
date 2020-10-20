6c1a70e438dbab9e8d9e9f19f24e1496e8fc8af0
Hey Yo! <50573855+Jkytol@users.noreply.github.com>
2020-10-20 19:01:16 +0300

    Add files via upload

---
+++ src/pages/Thank-you.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+import NameForm from "../components/afterform"
+import LandingBio from "../components/landing-bio"
+const ThanksPage = () => (
+  <Layout>
+
+    <LandingBio />
+    
+<NameForm />
+
+<div id="myDIVi"></div>
+<div id="myDIVii"><a href="https://paypal.me/kahviraha/5"><img src="https://raw.githubusercontent.com/Jkytol/JKgatsby/master/src/images/cGIF.gif" title="donateButton" /></a></div>
+
+  </Layout>
+)
+
+export default ThanksPage
