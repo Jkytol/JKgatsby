e28d6e3655d43162a8540fe6e91e3690a321a7f0
Hey Yo! <50573855+Jkytol@users.noreply.github.com>
2020-11-02 16:42:57 +0200

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const PageName = () => (
+  <Layout>
+    <SEO title="PageName" keywords={[`gatsby`, `application`, `react`]} />
+    <p>shitshitshit</p><p>shitshitshit</p><p>shitshitshit</p><p>shitshitshit</p>
+  </Layout>
+)
+
+export default PageName
