f5c2fe45ef1871351a920bbad04ea2d05a6321e4
Hey Yo! <50573855+Jkytol@users.noreply.github.com>
2020-11-02 19:24:45 +0200

    abbb

--- src/components/header.js
+++ src/components/header.js
@@ -77,6 +77,7 @@ const Header = ({ siteTitle }) => (
     <Content>
       <p>
         <HomeLink to="/">{siteTitle}</HomeLink>
+        <NavLink to="/about">About</NavLink>
         <NavLink to="/blog">Blog</NavLink>
         <GitHubLink href="https://github.com/kytol">
           GitHub
