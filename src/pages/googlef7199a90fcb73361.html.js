29b4770ad0478d0406775f80cbe627f86fd1237e
Hey Yo! <50573855+Jkytol@users.noreply.github.com>
2020-11-04 18:57:33 +0200

    Update and rename about.js to googlef7199a90fcb73361.html.js

--- src/pages/googlef7199a90fcb73361.html.js
+++ src/pages/googlef7199a90fcb73361.html.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const PageName = () => (
   <Layout>
     <SEO title="PageName" keywords={[`gatsby`, `application`, `react`]} />
-    <p>shitshitshit</p><p>shitshitshit</p><p>shitshitshit</p><p>shitshitshit</p>
+    google-site-verification: googlef7199a90fcb73361.html
   </Layout>
 )
 
