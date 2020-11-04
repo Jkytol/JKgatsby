ddb05076e3122eba0fa6749c96b893b3931198d7
Hey Yo! <50573855+Jkytol@users.noreply.github.com>
2020-11-04 19:00:01 +0200

    Update googlef7199a90fcb73361.html.js

--- src/pages/googlef7199a90fcb73361.html.js
+++ src/pages/googlef7199a90fcb73361.html.js
@@ -1,14 +1,1 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const PageName = () => (
-  <Layout>
-    <SEO title="PageName" keywords={[`gatsby`, `application`, `react`]} />
-    google-site-verification: googlef7199a90fcb73361.html
-  </Layout>
-)
-
-export default PageName
-
+google-site-verification: googlef7199a90fcb73361.html
